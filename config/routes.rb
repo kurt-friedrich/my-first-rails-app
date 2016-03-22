bcb86391dee77c02b64ade44ab61852726bd8f9c
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-03-22 15:08:44 -0400

    added lipsum action, routing, and view

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get '/:name' => 'welcome#name'
-  get '/lorem/:ipsum' => 'welcome#lipsum'
+  get '/lorem/:lipsum/(:num)' => 'welcome#lipsum'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
